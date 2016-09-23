6d6846c558e204a05c99e0d8fc0cd8d79cec0801
Peter LeBrun <peterlebrun@gmail.com>
2016-09-23 18:19:25 -0400

    Clean up dirty files

--- .emacs.d/init-config/init-keys.el
+++
@@ -1,4 +0,0 @@
-(global-set-key (kbd "C-, c") (lambda() (interactive) (find-file "~/.emacs.d/init.el")))
-(global-set-key (kbd "C-, l") (lambda() (interactive) (load-file "~/.emacs.d/init.el")))
-
-(provide 'init-keys)

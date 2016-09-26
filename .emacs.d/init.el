277526b2602de6b414893c12c705ccc720a7a1f4
Peter LeBrun <peterlebrun@gmail.com>
2016-09-26 13:23:20 -0400

    Only toggle fullscreen from keyboard

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,6 +46,7 @@
 (require 'init-zenburn-theme)
 (require 'linum-off)
 
+;; Don't edit these
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

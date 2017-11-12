7e506cbeb652b3235a9c24f30acb42afe7073f23
Peter LeBrun <peterlebrun@gmail.com>
2017-11-12 13:02:10 -0500

    Save file backups to specific directory, add .gitignore

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -114,3 +114,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old

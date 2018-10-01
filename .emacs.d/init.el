56f00917727b2a54f542bb0da31252a8f154de6a
Peter LeBrun <peterlebrun@gmail.com>
2018-10-01 13:43:41 -0400

    Some more formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,11 +3,21 @@
 ;;; Code:
 (require 'package)
 
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("org" . "https://orgmode.org/elpa/"))
+(add-to-list
+ 'package-archives
+ '("melpa" . "https://melpa.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("gnu" . "https://elpa.gnu.org/packages/"))
 
 (setq package-enable-at-startup nil)
 (package-initialize)

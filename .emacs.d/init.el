52cf09955cbc14d13714eb212037a4919d37599e
Peter LeBrun <peterlebrun@gmail.com>
2018-12-08 13:03:54 -0500

    ... so I can set up ORG BRAIN

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,6 +112,21 @@
 (use-package org
   :ensure t)
 
+(use-package org-brain :ensure t
+  :init
+  (setq org-brain-path "~/org-brain")
+  ;; For Evil users
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  ;(push '("b" "Brain" plain (function org-brain-goto-end)
+          ;"* %i%?" :empty-lines 1)
+        ;org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12))
+
 ;(use-package flycheck
   ;:ensure t
   ;:init (global-flycheck-mode))
@@ -240,7 +255,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-mode flycheck-yamllint flycheck dockerfile-mode puppet-mode yaml-mode company zenburn-theme powerline-evil powerline org-bullets magit exec-path-from-shell evil-indent-textobject evil-leader evil php-mode helm-projectile helm use-package))))
+    (org-brain org-mode flycheck-yamllint flycheck dockerfile-mode puppet-mode yaml-mode company zenburn-theme powerline-evil powerline org-bullets magit exec-path-from-shell evil-indent-textobject evil-leader evil php-mode helm-projectile helm use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

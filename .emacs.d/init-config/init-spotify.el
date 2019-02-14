67e6215293a6d1874068a2f2bf7000174e369fd3
Peter LeBrun <peterlebrun@gmail.com>
2019-02-13 19:33:38 -0500

    spotify customizations

--- .emacs.d/init-config/init-spotify.el
+++ .emacs.d/init-config/init-spotify.el
@@ -1,8 +1,15 @@
 ;; I really wish this package got into melpa...
 (add-to-list 'load-path (concat pbl-package-root "spotify.el/"))
+
+(setq spotify-mode-line-truncate-length 20)
+(setq spotify-mode-line-format "[%p %a - %t]")
+
+(setq spotify-mode-line-playing-text "▶")
+(setq spotify-mode-line-paused-text "‖")
+(setq spotify-mode-line-stopped-text "x")
+
 ;; @TODO: This is going to cause problems if emacs starts
 ;; without a network connection
-(setq spotify-mode-line-truncate-length 10)
 (require 'spotify)
 
 (provide 'init-spotify)

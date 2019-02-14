8d54fb538d52c956d6711efa297eaa7217091f04
Peter LeBrun <peterlebrun@gmail.com>
2019-02-14 10:36:23 -0500

    Minor changes to spotify mode line display

--- .emacs.d/init-config/init-spotify.el
+++ .emacs.d/init-config/init-spotify.el
@@ -1,8 +1,8 @@
 ;; I really wish this package got into melpa...
 (add-to-list 'load-path (concat pbl-package-root "spotify.el/"))
 
-(setq spotify-mode-line-truncate-length 20)
-(setq spotify-mode-line-format "|%p %a %t|")
+(setq spotify-mode-line-truncate-length 8)
+(setq spotify-mode-line-format "[%p %a: %t %R%S]")
 
 (setq spotify-mode-line-playing-text "▶")
 (setq spotify-mode-line-paused-text "‖")

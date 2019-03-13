e85f26f10ecc6207f8e346679bd9f591b952898a
Peter LeBrun <peterlebrun@gmail.com>
2019-03-12 21:53:38 -0400

    Create nice little syntactic sugar symlink

--- setup.sh
+++ setup.sh
@@ -13,3 +13,10 @@ for file in $dotfiles; do
     fi
     ln -s $PWD/$file $HOME/$file;
 done
+
+# a little syntactic sugar
+dotfiles_symlink=$HOME/eng/${PWD##*/}
+if [ -L $dotfiles_symlink ]; then
+    rm $dotfiles_symlink; # Clean out old symlink
+fi
+ln -s $PWD $dotfiles_symlink;

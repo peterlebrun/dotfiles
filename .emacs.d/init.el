f8e99dfa3e240ef1e45fa853a6af5c27ee00c4b8
Peter LeBrun <peterlebrun@gmail.com>
2020-10-28 16:16:24 -0400

    Do I even use this any more

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -97,20 +97,20 @@
 (use-package dockerfile-mode :ensure t)
 (use-package rjsx-mode :ensure t)
 
-(use-package org-brain :ensure t
-  :init
-  (setq org-brain-path "~/org-brain")
-  ;; For Evil users
-  (with-eval-after-load 'evil
-    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
-  :config
-  (setq org-id-track-globally t)
-  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
-  ;(push '("b" "Brain" plain (function org-brain-goto-end)
-          ;"* %i%?" :empty-lines 1)
-        ;org-capture-templates)
-  (setq org-brain-visualize-default-choices 'all)
-  (setq org-brain-title-max-length 12))
+; (use-package org-brain :ensure t
+  ; :init
+  ; (setq org-brain-path "~/org-brain")
+  ; ;; For Evil users
+  ; (with-eval-after-load 'evil
+    ; (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  ; :config
+  ; (setq org-id-track-globally t)
+  ; (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  ; ;(push '("b" "Brain" plain (function org-brain-goto-end)
+          ; ;"* %i%?" :empty-lines 1)
+        ; ;org-capture-templates)
+  ; (setq org-brain-visualize-default-choices 'all)
+  ; (setq org-brain-title-max-length 12))
 
 (set-face-attribute 'default nil :font "FuraCode Nerd Font 18")
 

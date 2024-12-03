80163bff38abe86261ca4d0d64e7326457465e5e
Peter LeBrun <peterlebrun@gmail.com>
2024-12-02 21:44:49 -0500

    Add css mode

--- .emacs.d/init-config/init-yaml-mode.el
+++ .emacs.d/init-config/init-yaml-mode.el
@@ -1,3 +1,8 @@
 (use-package yaml-mode :ensure t)
+(use-package css-mode :ensure t)
+
+(add-hook 'css-mode-hook
+          (lambda ()
+            (display-line-numbers-mode 1)))
 
 (provide 'init-yaml-mode)

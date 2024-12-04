f1b4f93dc02bfed85efa7dc1cb6a0e81e1437dc3
Peter LeBrun <peterlebrun@gmail.com>
2024-12-03 19:32:21 -0500

    Speed up counsel-projectile search

--- .emacs.d/init-config/init-counsel.el
+++ .emacs.d/init-config/init-counsel.el
@@ -10,12 +10,16 @@
               ("C-c p" . projectile-command-map)))
 
 (use-package counsel-projectile
-  :ensure t)
+  :ensure t
+  :config
+  (setq projectile-enable-caching t
+        projectile-completion-system 'ivy
+        projectile-indexing-method 'native)
+  (add-to-list 'projectile-globally-ignored-directories "*venv"))
 
 (use-package ivy :demand
   :config
   (setq ivy-use-virtual-buffers t
         ivy-count-format "%d/%d "))
-
 
 (provide 'init-counsel)

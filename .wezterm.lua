880e6be4f2edccb3761d58e6e0be03a921cdf496
Peter LeBrun <peterlebrun@gmail.com>
2025-05-20 16:26:40 -0400

    Update some colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,11 @@
 local wezterm = require 'wezterm'
 
 return {
-    font = wezterm.font('Fira Code', {weight='Light'}),
+    font = wezterm.font('Fira Code', { weight='Regular' }),
     font_size = 18,
     initial_rows = 30,
     initial_cols = 120,
+    colors = {
+        background = '#333',
+    },
 }
